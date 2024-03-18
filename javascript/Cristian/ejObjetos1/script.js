187a2d1c7b6bceed78767ab115b9dbe31444089d
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-18 11:28:20 +0100

    Completa ejercicio 1 de objetos dado por Cristian

--- javascript/Cristian/ejObjetos1/script.js
+++ javascript/Cristian/ejObjetos1/script.js
@@ -1,3 +1,12 @@
 function ejercicio1 () {
-    
+    const alumno = {
+        "nombre": "Juan Manuel",
+        "ingles": 10,
+        "programacion": 10,
+        "html": 10
+    }
+
+    const media = (alumno.ingles + alumno.programacion + alumno.html) / 3;
+    let solucion = `El alumno ${alumno.nombre} tiene una media de ${media}`
+    document.getElementById("ej1-Output").innerText = solucion;
 }
