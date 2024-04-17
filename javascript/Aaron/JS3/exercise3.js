c410db0c797b0014d0d04149bcb2250892e691e8
JK-B0T <JK-B0T@users.noreply.github.com>
2024-04-17 14:18:57 +0200

    Avanza práctica 3

--- javascript/Aaron/JS3/exercise3.js
+++ javascript/Aaron/JS3/exercise3.js
@@ -2,3 +2,51 @@
 
 // Edita sólo este fichero
 
+window.addEventListener("DOMContentLoaded", main, false);
+
+function main () {
+    const imgPreview = document.querySelector("#imgPreview");
+    const input = document.querySelector("#image");
+    const form = document.querySelector("#newEvent");
+
+    const inputArray = Array.from(document.querySelectorAll("input:not([type='button'])"));
+    inputArray.push(document.querySelector("textarea"));
+
+    form.addEventListener("submit", checkInputsValidation, false);
+
+    function checkInputsValidation() {
+        let isFormComplete = true;
+        inputArray.map((element) => {
+            if (element.value === null && !element.classList.contains("is-invalid")) {
+                element.classList.toggle("is-invalid");
+                if (element.classList.contains("is-valid")) {
+                    element.classList.toggle("is-valid");
+                }
+                isFormComplete = false;
+            } else if (element.value !== null && !element.classList.contains("is-valid")) {
+                if (element.classList.contains("is-invalid")) {
+                    element.classList.toggle("is-invalid");
+                }
+                element.classList.toggle("is-valid");
+            }
+
+            if (isFormComplete = true) {
+                resertform();
+            }
+        });
+    }
+
+    input.addEventListener("change", (e) => {
+        const file = input.files[0];
+
+        if (file.type.startsWith("image")) {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+
+            reader.addEventListener("load", e => {
+                imgPreview.classList.remove("d-none");
+                imgPreview.src = reader.result;
+            })
+        }
+    }, false);
+}
