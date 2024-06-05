428d976f42135b86e8dcf1a6886a45fd9a25afec
JK-B0T <JK-B0T@users.noreply.github.com>
2024-06-05 14:12:07 +0200

    Añade renderización de proyectos

--- javascript/Aaron/JS4/ejercicio_4.2/IUScript.js
+++ javascript/Aaron/JS4/ejercicio_4.2/IUScript.js
@@ -1,7 +1,8 @@
 window.addEventListener("DOMContentLoaded", start, false) 
 
 function start() {
-    const bandeja = document.querySelector("aside");
+    let listaProyecto = {"bandeja": document.querySelector("aside")} 
+
     const dialogList = [
         document.getElementById("nuevoCoordinador"),
         document.getElementById("nuevoEstudiante"),
@@ -17,10 +18,6 @@ function start() {
     const btnNuevoProyecto = document.getElementById("btnNuevoProyecto");
     btnNuevoProyecto.addEventListener("click", () => dialogList[2].showModal(), false);
 
-    const est = new Estudiante("Pepo");
-    const cor = new Coordinador("Coco", "Cocotero");
-    const pro = new Proyecto("ZXY123", 3, 10);
-
     function sumarHoras() {
         console.log("Horas añadidas");
     }
@@ -30,7 +27,7 @@ function start() {
     }
 
     function renderizarEstudiante(nombreTabla, estudiante, indice) {
-        const position = document.querySelector(`${nombreTabla} div:nth-child(${indice})`);
+        const posicion = document.querySelector(`${nombreTabla} div:nth-child(${indice})`);
         
         const div = document.createElement("div");
         div.classList.add("estudiante");
@@ -71,12 +68,102 @@ function start() {
         if (indice === 0) {
             document.querySelector(`${nombreTabla}`).prepend(div);
         } else {
-            position.insertAdjacentElement("afterend", div);
+            posicion.insertAdjacentElement("afterend", div);
         }
     }
 
+    function renderizarProyecto(proyecto, indice) {
+        const posicion = document.querySelector(`main article:nth-child(${indice})`);
+        
+        const article = document.createElement("article");
+        const header = document.createElement("header");
+        const section = document.createElement("section");
+        section.setAttribute("id", proyecto.getCodigo());
+
+        const span1 = document.createElement("span");
+        span1.classList.add("codigoProyecto");
+        span1.textContent = proyecto.getCodigo();
+        header.appendChild(span1);
+        
+        const span2 = document.createElement("span");
+        span2.classList.add("maxEstudiantes");
+        span2.textContent = "Max: " + proyecto.maxEstudiantes;
+        header.appendChild(span2);
+
+        const span3 = document.createElement("span");
+        span3.classList.add("horasInvertidas");
+        span3.textContent = "Horas: " + proyecto.getHoras();
+        header.appendChild(span3);
+
+        const btn1 = document.createElement("button");
+        btn1.setAttribute("type", "button")
+        btn1.classList.add("closeBtn");
+        btn1.textContent = "X";
+        btn1.addEventListener("click", () => eliminarRenderizado(article), false)
+        header.appendChild(btn1);
+
+        article.appendChild(header);
+        article.appendChild(section);
+
+        if (indice === 0) {
+            document.querySelector(`main`).prepend(article);
+        } else {
+            posicion.insertAdjacentElement("afterend", article);
+        }
+    }
+
+    const est = new Estudiante("Pepo");
+    const cor = new Coordinador("Coco", "Cocotero");
+    const pro1 = new Proyecto("ABC123", 3, 10);
+    const pro2 = new Proyecto("DFG456", 3, 10);
+    const pro3 = new Proyecto("HYJ789", 3, 10);
+    const pro4 = new Proyecto("KLM012", 3, 10);
+    const pro5 = new Proyecto("NOP345", 3, 10);
+
+    renderizarProyecto(pro1, 0);
+    renderizarProyecto(pro2, 1);
+    renderizarProyecto(pro3, 2);
+    renderizarProyecto(pro4, 3);
+    renderizarProyecto(pro5, 4);
+
+    renderizarEstudiante("#"+pro1.getCodigo(), cor, 0);
+    renderizarEstudiante("#"+pro1.getCodigo(), est, 1);
+    renderizarEstudiante("#"+pro1.getCodigo(), est, 2);
+    renderizarEstudiante("#"+pro1.getCodigo(), est, 3);
+
+    renderizarEstudiante("#"+pro2.getCodigo(), cor, 0);
+    renderizarEstudiante("#"+pro2.getCodigo(), est, 1);
+    renderizarEstudiante("#"+pro2.getCodigo(), est, 2);
+    renderizarEstudiante("#"+pro2.getCodigo(), est, 3);
+    renderizarEstudiante("#"+pro2.getCodigo(), est, 4);
+    renderizarEstudiante("#"+pro2.getCodigo(), est, 5);
+    renderizarEstudiante("#"+pro2.getCodigo(), est, 6);
+
+    renderizarEstudiante("#"+pro3.getCodigo(), cor, 0);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 1);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 2);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 3);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 4);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 5);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 6);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 7);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 8);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 9);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 10);
+    renderizarEstudiante("#"+pro3.getCodigo(), est, 11);
+
+    renderizarEstudiante("#"+pro4.getCodigo(), cor, 0);
+    renderizarEstudiante("#"+pro4.getCodigo(), est, 1);
+
     renderizarEstudiante("aside", est, 0);
-    renderizarEstudiante("aside", cor, 2);
+    renderizarEstudiante("aside", est, 0);
+    renderizarEstudiante("aside", est, 0);
+    renderizarEstudiante("aside", est, 0);
+    renderizarEstudiante("aside", est, 0);
+    renderizarEstudiante("aside", est, 0);
+    renderizarEstudiante("aside", est, 0);
+    renderizarEstudiante("aside", est, 0);
+    renderizarEstudiante("aside", cor, 1);
 
     function renderizarCoodinador() {
         
