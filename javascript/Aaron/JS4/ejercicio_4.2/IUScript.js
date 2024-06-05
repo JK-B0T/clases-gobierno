d8905c99797c5712aa59b10b1ace5b09e9d47690
JK-B0T <JK-B0T@users.noreply.github.com>
2024-06-05 12:06:42 +0200

    Añade funcionalidad para botones de estudiante

--- javascript/Aaron/JS4/ejercicio_4.2/IUScript.js
+++ javascript/Aaron/JS4/ejercicio_4.2/IUScript.js
@@ -6,6 +6,10 @@ function start() {
     const cor = new Coordinador("Coco", "Cocotero");
     const pro = new Proyecto("ZXY123", 3, 10);
 
+    function sumarHoras() {
+        console.log("Horas añadidas");
+    }
+
     function renderizarEstudiante(nombreTabla, estudiante, indice) {
         const position = document.querySelector(`${nombreTabla} div:nth-child(${indice})`);
         
@@ -30,6 +34,20 @@ function start() {
         span3.classList.add("nombreEstudiante");
         span3.textContent = estudiante.getNombre();
         div.appendChild(span3);
+
+        const btn1 = document.createElement("button");
+        btn1.setAttribute("type", "button")
+        btn1.classList.add("btnElminarEstudiante");
+        btn1.textContent = "X";
+        btn1.addEventListener("click", () => div.parentNode.removeChild(div), false)
+        div.appendChild(btn1);
+
+        const btn2 = document.createElement("button");
+        btn2.setAttribute("type", "button")
+        btn2.classList.add("btnSumarHoras");
+        btn2.textContent = "+";
+        btn2.addEventListener("click", sumarHoras, false)
+        div.appendChild(btn2);
 
         if (indice === 0) {
             document.querySelector(`${nombreTabla}`).prepend(div);
